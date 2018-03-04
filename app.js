b3826cff50bf7a183aea81bace36751f5a452269
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-03-04 17:22:07 -0500

    rendered hello world by adding layout.hbs, index.hbs, and setting view engine to hbs

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const parser = require("body-parser");
 const passport = require("passport");
 
 const app = express();
+
+// set view to hbs
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 // test server
 app.listen(3000, (req, res) => {
   console.log("Server is working. Get ready for some sweet manga artwork!");
--- todo.md
+++ todo.md
@@ -1,1 +1,3 @@
 install dependencies, require them in app.js, do hello world on nodemon
+
+render a page with hello world in hbs.
