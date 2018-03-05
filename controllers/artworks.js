4e41dd82aa1c7bb6b8578ec13f0493707db23922
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-03-05 13:37:02 -0500

    created new.hbs, made route to it, made a short form on the page

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+// this gets the model. for some reason, can't do it by importing connection.js
 const Artwork = require("../models/Artworks");
 
 router.get("/", (req, res) => {
-  console.log("hello");
+  Artwork.find({}).then(artworks => {
+    res.render("index", { artworks });
+  });
+});
+
+router.get("/new", (req, res) => {
+  res.render("CRUD/new");
 });
 
 module.exports = router;
